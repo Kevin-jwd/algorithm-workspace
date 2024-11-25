f85bb0eb0924627277ff04df32db9235ac58bfac
DESKTOP-D0SBUEF\USER <herobine159@naver.com>
2024-11-25 19:22:11 +0900

    프로그래머스 코딩 테스트 입문 '짝수 홀수 개수' 2가지 방법에 따라 구현
    - solution1: forEach메서드 내의 if문을 통해 짝수와 홀수를 판별하고 각각 evenCount, oddCount를 증가
                          answer 배열에 최종 evenCount, oddCount 값을 push하고 반환
    - solution2: reduce 메서드를 활용하여  num_list를 순회하며 [0,0]으로 초기화된 배열을 1씩 증가시키고 반환

---
+++ introduction/짝수 홀수 개수.js
@@ -0,0 +1,22 @@
+function solution1(num_list) {
+    var answer = [];
+    var evenCount = 0;
+    var oddCount = 0;
+    num_list.forEach(function (value) {
+        if (value % 2 === 0) evenCount += 1;
+        else oddCount += 1;
+    });
+    answer.push(evenCount);
+    answer.push(oddCount);
+    return answer;
+}
+
+function solution2(num_list) {
+    return num_list.reduce(
+        ([evenCount, oddCount], value) =>
+            value % 2 === 0 
+                ? [evenCount + 1, oddCount] 
+                : [evenCount, oddCount + 1],
+        [0, 0]
+    );
+}
